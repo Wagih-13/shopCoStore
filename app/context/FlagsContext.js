5a1112336e73d66b347b5f5cbb455d5183361ec9
ahmed wagih <144692343+Wagih-13@users.noreply.github.com>
2025-08-01 23:35:59 +0300

    Update FlagsContext.js
    change wrong code with other fixed code

--- app/context/FlagsContext.js
+++ app/context/FlagsContext.js
@@ -1,3 +1,21 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9201616d0ffa6ec0e664f3b4efe21c77dc3d9dcee62b31a39b5063d473a732a2
-size 570
+"use client";
+import { createContext, useContext, useState } from "react";
+
+// Define the initial context structure
+const FlagsContext = createContext();
+
+const FlagsContextProvider = ({ children }) => {
+  const [displayCart, setDisplayCart] = useState(false);
+  const [displayFilter, setDisplayFilter] = useState(false);
+  return (
+    <FlagsContext.Provider
+      value={{ displayCart, displayFilter, setDisplayCart, setDisplayFilter }}
+    >
+      {children}
+    </FlagsContext.Provider>
+  );
+};
+
+export default FlagsContextProvider;
+export const useFlagsContext = () => useContext(FlagsContext);
+
